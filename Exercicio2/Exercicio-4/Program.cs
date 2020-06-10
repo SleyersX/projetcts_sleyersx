951dc4180cd9512e905cedbb423a57040943cf91
Walter Moura <walter.araujo.dw@gmail.com>
2020-06-10 07:43:35 -0300

    Update Curso CSharp

---
+++ Exercicio2/Exercicio-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_4 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com dois números inteiros ");
+            Console.Write("Primeiro número: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Segundo número: ");
+            int B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+
+            Console.WriteLine("Soma de {0} + {1} é igual á {2}", A, B, SOMA);
+
+        }
+    }
+}
---
+++ Exercicio2/Exercicio-5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_5 {
+    class Program {
+        static void Main(string[] args) {
+
+            double pi = 3.14159, area, resultado;
+
+            Console.Write("Entre com valor da área ser calculada [A]: ");
+            area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            resultado = (pi * area * area);
+            Console.WriteLine("Área [A] é igual {0}", resultado.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
