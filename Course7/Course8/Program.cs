5fc36f2cb85be409fe61703c12b962d75c137f24
Walter Moura <walter.araujo.dw@gmail.com>
2020-07-10 23:02:18 -0300

    updates

---
+++ Course7/Course8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Course8 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+            int x = 10;
+
+            Console.WriteLine("Bom dia");
+
+            if (x > 5) {
+                Console.WriteLine("Boa tarde");
+            }
+                        
+            Console.WriteLine("Boa noite");
+            */
+            Console.WriteLine("Entre com um númeiro inteiro:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("Par!");
+            } else {
+                Console.WriteLine("Impar!");
+            }
+
+        }
+    }
+}
