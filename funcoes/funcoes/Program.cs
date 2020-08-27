58e224a9bea3a903098b1c1e0dde16d9cf07f28f
Walter Moura <walter.araujo.dw@gmail.com>
2020-08-26 22:53:55 -0300

    Update aula funções

--- funcoes/funcoes/Program.cs
+++ funcoes/funcoes/Program.cs
@@ -3,7 +3,25 @@
 namespace funcoes {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite três números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("Maior = " + resultado);
+
+        }
+
+        static int Maior(int a, int b, int c) {
+            if (a > b && a > c) {
+                return a;
+            } else if (b > c) {
+                return b;
+            } else {
+                return c;
+            }
         }
     }
 }
