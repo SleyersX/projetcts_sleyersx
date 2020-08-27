bcbbfb24095063d1a231a9a3788e8b0aabc515ba
Walter Moura <walter.araujo.dw@gmail.com>
2020-08-26 22:29:31 -0300

    Updates CursoCSharp

---
+++ Course8/Course8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Course8 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Ente com um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("Par!");
+            } 
+            else {
+                Console.WriteLine("Impar!");
+            }
+        }
+    }
+}
