94ca07f316dfd911b4fbe266ce70cd2bc444cf34
Walter Moura <walter.araujo.dw@gmail.com>
2020-10-17 01:50:47 -0300

    Updates

---
+++ GerarDigitoEAN/GerarDigitoEAN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace GerarDigitoEAN {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o código EAN: ");
+            string str = Console.ReadLine();
+
+            char[] b = new char[str.Length];
+            using (StringReader sr = new StringReader(str)) {
+                sr.Read(b, 0, str.Length);
+                Console.WriteLine(b);
+
+            }
+
+        }
+    }
+}
